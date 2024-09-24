5d17c96d934bd199021b1d7c51f24c292587380d
Joe Thomson <joseph_2166@mac.com>
2024-09-24 12:41:28 +0100

    Added new asyncStream variable to Notification.Name

--- SwiftyNotifications/NotificationName+Helpers.swift
+++ SwiftyNotifications/NotificationName+Helpers.swift
@@ -42,6 +42,20 @@ public extension Notification.Name
     {
         NotificationCenter.default.post(name: self, object: nil)
     }
+    
+    /// An `AsyncStream` that yields every time a notification with this name is posted to the default notification center.
+    @available(iOS 15, macCatalyst 15, macOS 12, tvOS 15, watchOS 8, *)
+    var asyncStream: AsyncStream<Notification.Name> {
+        AsyncStream { continuation in
+            let task = Task {
+                for await notification in NotificationCenter.default.notifications(named: self)
+                {
+                    continuation.yield(self)
+                }
+            }
+            continuation.onTermination = { _ in task.cancel() }
+        }
+      }
 }
 
 extension Notification.Name: @retroactive ExpressibleByStringLiteral
